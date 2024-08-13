9f7f0416d108c16c1741e7148dc1b1a045eeb9e8
KoushikReddy24 <124913094+KoushikReddy24@users.noreply.github.com>
2024-08-13 13:45:55 +0530

    Add files via upload

---
+++ js/myTest.js
@@ -0,0 +1,10 @@
+describe('Testing Java script',function(){
+    it('first unit test',function(){
+        let testStr ='my-test';
+        expect(testStr).toBe('my-test');
+    })
+    it('second unit test',function(){
+        let testStr ='my-test2';
+        expect(testStr).toBe('my-test2');
+    })
+})
